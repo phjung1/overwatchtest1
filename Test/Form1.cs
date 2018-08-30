112af55f08671bb96534453d89adf965b2b63a81
phjung1 <36128943+phjung1@users.noreply.github.com>
2018-08-30 09:25:51 +0900

    초기 커밋.

---
+++ Test/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ScreenStateLogger screenStateLogger = new ScreenStateLogger();
+
+        Timer t = new Timer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            int width = 1024;
+            int height = 768;
+            Color[,] colorArray = new Color[width, height];
+
+
+            
+
+            screenStateLogger.ScreenRefreshed += (s, data) =>
+            {
+
+              
+
+                    MyMouse mouse = new MyMouse();
+                    Bitmap bmp;
+                    LockBitmap lockBitmap;
+
+                    double moveX = 0;
+                    double moveY = 0;
+                    int areaX = width / 2;
+                    int areaY = height / 2;
+
+                    int offset = 100;
+
+                    int startX = areaX - offset;
+                    int startY = areaY - offset;
+                    int endX = areaX + offset;
+                    int endY = areaY + offset;
+                    Color c;
+
+                    Point p;
+
+
+                    using (var ms = new MemoryStream(data))
+                    {
+
+                        bmp = new Bitmap(Image.FromStream(ms));
+                        lockBitmap = new LockBitmap(bmp);
+                        lockBitmap.LockBits();
+
+
+
+                        for (int y = startY; y < endY - 1; y++)
+                        {
+                            for (int x = startX; x < endX - 1; x++)
+                            {
+                                c = lockBitmap.GetPixel(x, y);
+                                bool b1 = c.R > 200 && c.B < 160 && c.G < 160;
+                                bool b2 = c.R > 150 && c.B < 80 && c.G < 95;
+
+                                bool bb1 = c.R > 180 && c.G > 140;
+
+                                if (bb1)
+                                {
+                                    continue;
+                                }
+
+                                if (b1 || b2)
+                                {
+                                    moveX = (x - width / 2);
+                                    moveY = (y - height / 2) - 30;
+
+                                    break;
+                                }
+
+
+                            }
+                        }
+                        lockBitmap.UnlockBits();
+                        p = new Point(moveX, moveY);
+                        mouse.Move(p);
+
+                    }
+              
+            };
+            screenStateLogger.Start();
+
+
+            //globalKeyboardHook gkh = new globalKeyboardHook();
+            //gkh.HookedKeys.Add(Keys.F7);
+            //gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            //gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+            //gkh.hook();
+        }
+
+
+
+        private void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Test/MyMouse.cs
@@ -0,0 +1,134 @@
+﻿//필요한 네임스페이스
+using System;
+using System.Runtime.InteropServices; // DllImport를 위한 네임스페이스
+
+
+
+[Flags()]
+public enum MouseEventFlag : int
+{
+    Absolute = 0x8000,
+    LeftDown = 0x0002,
+    LeftUp = 0x0004,
+    MiddleDown = 0x0020,
+    MiddleUp = 0x0040,
+    Move = 0x0001,
+    RightDown = 0x0008,
+    RightUp = 0x0010,
+    Wheel = 0x0800,
+    XDown = 0x0080,
+    XUp = 0x0100,
+    HWheel = 0x1000,
+}
+
+
+
+public enum MouseButton
+{
+    Left,
+    Right,
+    Middle,
+    X,
+}
+
+
+public class Size
+{
+    public double Width { set; get; }
+    public double Height { set; get; }
+
+    public Size() { }
+    public Size(double Width, double Height)
+    { this.Width = Width; this.Height = Height; }
+}
+
+
+
+public struct Point
+{
+    public double X { set; get; }
+    public double Y { set; get; }
+
+    public Point(double X, double Y)
+     : this()
+    { this.X = X; this.Y = Y; }
+}
+
+
+public class MyMouse
+{
+    // API함수를 C#에서 사용할 수 있도록 합니다.
+    #region DllImport
+    [DllImport("User32.dll", CharSet = CharSet.Auto)]
+ private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
+    #endregion
+
+    // 절대적인 좌표로 마우스를 움직일때, 약간 특수한 계산이 필요합니다.
+    #region Constant
+    const int ABSOLUTE_SIZE = 65535;
+    #endregion
+
+    // 절대적인 좌표로 마우스를 움질일때, 화면 크기를 꼭 알아야합니다.
+    #region Property
+    public Size DisplaySize { set; get; }
+    #endregion
+
+    // 마우스를 제어하는 함수들입니다.
+    #region Moving
+    public void Move(Point Point)
+    {
+        MouseEventFlag Flag = MouseEventFlag.Move;
+
+        mouse_event((int)Flag, (int)Point.X, (int)Point.Y, 0, IntPtr.Zero);
+    }
+
+    public void MoveAt(Point Point)
+    {
+        MouseEventFlag Flag = MouseEventFlag.Move | MouseEventFlag.Absolute;
+
+        int X = (int)(ABSOLUTE_SIZE / 1024 * Point.X);
+        int Y = (int)(ABSOLUTE_SIZE / 768 * Point.Y);
+
+        mouse_event((int)Flag, X, Y, 0, IntPtr.Zero);
+    }
+
+    public void MoveAbsolute(Point Point)
+    {
+        MouseEventFlag Flag = MouseEventFlag.Move | MouseEventFlag.Absolute;
+
+        mouse_event((int)Flag, (int)Point.X, (int)Point.Y, 0, IntPtr.Zero);
+    }
+    #endregion
+
+    #region Input
+    public void Down(MouseButton Button)
+    {
+        MouseEventFlag Flag = 0;
+
+        switch (Button)
+        {
+            case MouseButton.Left: Flag = MouseEventFlag.LeftDown; break;
+            case MouseButton.Right: Flag = MouseEventFlag.RightDown; break;
+            case MouseButton.Middle: Flag = MouseEventFlag.MiddleDown; break;
+            case MouseButton.X: Flag = MouseEventFlag.XDown; break;
+        }
+
+        mouse_event((int)Flag, 0, 0, 0, IntPtr.Zero);
+    }
+
+    public void Up(MouseButton Button)
+    {
+        MouseEventFlag Flag = 0;
+
+        switch (Button)
+        {
+            case MouseButton.Left: Flag = MouseEventFlag.LeftUp; break;
+            case MouseButton.Right: Flag = MouseEventFlag.RightUp; break;
+            case MouseButton.Middle: Flag = MouseEventFlag.MiddleUp; break;
+            case MouseButton.X: Flag = MouseEventFlag.XUp; break;
+        }
+
+        mouse_event((int)Flag, 0, 0, 0, IntPtr.Zero);
+    }
+    #endregion
+}
